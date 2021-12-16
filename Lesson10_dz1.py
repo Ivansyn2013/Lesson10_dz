d76d41961e1bd3d55750e83d9f12ec33ec5f6f06
ivan <ivansyn2013@yandex.ru>
2021-12-16 13:13:56 +0300

    Сделано домашнее задание

--- Lesson10_dz1.py
+++ Lesson10_dz1.py
@@ -9,6 +9,7 @@ def my_list(x, y):
     else:
         return x + y
 
+
 class Matrix:
     def __init__(self, list):
         self.list = list
@@ -17,34 +18,31 @@ class Matrix:
         for el in self.list:
             print(*IterMatList(el))
         return ''
-        #return f'{[IterMatList(el) for el in self.list]}' как это реализовать?
+        # return f'{[IterMatList(el) for el in self.list]}' как это реализовать?
+
     def __add__(self, other):
         return Matrix(my_list(self.list, other.list))
-
-
-
 
 
 class IterMatList:
     def __init__(self, start):
         self.st = start
         self.i = 0
+
     def __iter__(self):
         return self
 
     def __next__(self):
         if self.i < len(self.st):
-            self.i +=1
-            return self.st[self.i-1]
+            self.i += 1
+            return self.st[self.i - 1]
         else:
             raise StopIteration
 
 
-
-
-mc_list1 = [[31,22],[37,43],[51,86]]
-mc_list2 = [[3,5,32],[2,4,6],[-1,64,-8]]
-mc_list3 = [[3,5,8,3],[8,3,7,1]]
+mc_list1 = [[31, 22], [37, 43], [51, 86]]
+mc_list2 = [[3, 5, 32], [2, 4, 6], [-1, 64, -8]]
+mc_list3 = [[3, 5, 8, 3], [8, 3, 7, 1]]
 
 mc1 = Matrix(mc_list1)
 mc2 = Matrix(mc_list2)
@@ -53,4 +51,4 @@ mc3 = Matrix(mc_list3)
 print(mc1)
 print(mc2)
 print(mc3)
-print(mc1+mc2)
+print(mc1 + mc2)
--- Lesson10_dz2.py
+++ Lesson10_dz2.py
@@ -1,21 +1,30 @@
-class Clothes:
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
     def __init__(self, param):
         self.param = param
+
     def __add__(self, other):
         return f'Общий объем ткани {self.coat_t + other.costume_t}'
+
+    @abstractmethod
+    def tisue(self):
+        pass
 
 
 class Coat(Clothes):
     @property
     def tisue(self):
-        self.coat_t = self.param/6.5 + 0.5
-        return f'Нужно ткани на пальто {self.param/6.5 + 0.5}'
+        self.coat_t = self.param / 6.5 + 0.5
+        return f'Нужно ткани на пальто {self.param / 6.5 + 0.5}'
 
-class Costume(Clothes) :
+
+class Costume(Clothes):
     @property
     def tisue(self):
-        self.costume_t = self.param*2 +0.3
-        return f'Нужно ткани на костюм {self.param*2 +0.3}'
+        self.costume_t = self.param * 2 + 0.3
+        return f'Нужно ткани на костюм {self.param * 2 + 0.3}'
 
 
 coat1 = Coat(10)
@@ -23,4 +32,4 @@ costume1 = Costume(20)
 t = coat1
 print(costume1.tisue)
 print(coat1.tisue)
-print(coat1+costume1)
+print(coat1 + costume1)
